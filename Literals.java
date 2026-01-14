12802a0045a38e1c85d60f57304472bdba83e0c4
Nivedita Sen <niveditasen14@gmail.com>
2026-01-15 00:16:41 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+    public static void main(String[] arg){
+        byte age = 34;
+        int page = 6;
+        char gender = 'M';
+        float f1 = 7.8f;
+        double d1 = 4.55d;
+        System.out.println(age);
+        System.out.println(page);
+        System.out.println(gender);
+        System.out.println(f1);
+        System.out.println(d1);
+
+    }
+
+}
