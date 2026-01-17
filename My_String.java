f82b385aa94fcd89610de52d6793b70cefb54f62
Nivedita Sen <niveditasen14@gmail.com>
2026-01-17 21:51:15 +0530

    Add files via upload

---
+++ My_String.java
@@ -0,0 +1,16 @@
+
+
+public class My_String {
+    public static void main(String[] args){
+        String name = "Mili";
+        System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
+    }
+
+}
+
