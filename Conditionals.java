1b6d2f05a0e70303d2c4bb5e6ea1a872e9a9c975
Nivedita Sen <niveditasen14@gmail.com>
2026-01-20 23:17:22 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Conditionals {
+    public static void main(String[] args){
+        int age = 56;
+
+        if(age>=18){
+            System.out.println("Congrats !!! You are eligible to drive and vote.");   
+        }
+        else{
+            System.out.println("You are not eligible to drive and vote. Go and watch cartoons.");
+        }
+    }
+    
+}
