3cbad738d3a50f72127fe2090c16504ec71b32b1
Nivedita Sen <niveditasen14@gmail.com>
2026-01-25 22:35:13 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,22 @@
+public class Numbers {
+    public static void main(String[] args){
+        int i;
+        for(i=0;i<=10;i++){
+            System.out.println(i);
+            //Natural Numbers    
+        }
+        System.out.println("Natural Numbers");
+        for(i=0;i<=10;i++){
+            System.out.println(2*i);
+            //Even Numbers
+        }
+        System.out.println("Even Numbers");
+        for(i=0;i<=10;i++){
+            System.out.println(2*i+1);
+            //Odd Numbers
+        }
+        System.out.println("Odd Numbers");
+        
+    }
+    
+}
