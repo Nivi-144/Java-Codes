1f19bc688e5925dc50140e4fb642f8e02c258804
Nivedita Sen <niveditasen14@gmail.com>
2026-02-14 21:42:58 +0530

    Add files via upload

---
+++ Meow.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Meow{
+    public static void main(String[] args){
+        int sum=0;
+        int n=3;
+        for(int i =0;i<n;i++){
+            sum=sum+(2*i);
+        }
+        System.out.print("Sum of even Numbers is:");
+        System.out.println(sum);
+
+    }
+
+}
