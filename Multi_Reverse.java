f9ca2da39ad9663ba283ec6e4833e66e8843ef86
Nivedita Sen <niveditasen14@gmail.com>
2026-02-14 21:55:09 +0530

    Add files via upload

---
+++ Multi_Reverse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Multi_Reverse{
+    public static void main(String[] args){
+       int n= 5;
+       
+       for (int i=10;i>=1;i--){
+            System.out.printf("%d * %d = %d\n", n, i, n*i);
+       }
+
+    }
+
+}
