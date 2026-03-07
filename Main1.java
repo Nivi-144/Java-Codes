840e66c576cbefed325a52dd786c547dd1b108df
Nivedita Sen <niveditasen14@gmail.com>
2026-03-07 16:41:27 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main(String args[]){
+        int a=12;
+        int b=16;
+        int c=19;
+        int sum = a+b+c;
+        System.out.println("Sum : "+sum);
+
+    }
+}
