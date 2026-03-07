ca7f8475e92fa3722d4bbc0e32a66a508d9325f0
Nivedita Sen <niveditasen14@gmail.com>
2026-03-07 16:37:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Sunday");
+        System.out.println("Monday");
+        System.out.println("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.println("Saturday");
+    }
+}
