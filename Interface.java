2c3d46bd7c79f04c94ed469bb405fdc280b8c55c
Nivedita Sen <niveditasen14@gmail.com>
2026-04-29 23:05:35 +0530

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,14 @@
+interface Animal{
+    void sound();
+}
+class Dog implements Animal{
+    public void sound(){
+        System.out.println("Bark");
+    }
+}
+public class Interface{
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound();
+    }
+}
