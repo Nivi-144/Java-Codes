be19cc71ccaae7087c578e40520c37b0d6f9acb5
Nivedita Sen <niveditasen14@gmail.com>
2026-04-29 22:52:45 +0530

    Add files via upload

---
+++ Mickey.java
@@ -0,0 +1,18 @@
+
+class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking");
+    }
+}
+public class Mickey{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
