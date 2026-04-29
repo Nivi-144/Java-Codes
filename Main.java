959b239c6f5382feac3a239f1270e06bb522564b
Nivedita Sen <niveditasen14@gmail.com>
2026-04-29 22:45:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
-class car{
-    String brand;
-    int speed;
-    void display(){
-        System.out.println("Brand:"+brand+",Speed:"+speed);
+class Car{
+    String name;
+    int model_num;
+
+    Car(String n,int a){
+        name=n;
+        model_num= 3;
     }
-}
+    void display(){
+        System.out.println(name + "" + model_num);
+    }
+} 
 public class Main{
-    public static void main(String [] args){
-        car c1=new car();
-        c1.brand="Maruti";
-        c1.speed=140;
+    public static void main(String[] args){
+        Car c1= new Car("Audi_S",4);
         c1.display();
     }
+    
 }
